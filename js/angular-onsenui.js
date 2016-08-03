aa8dceac09adcb1d124d3f7ca3e4a4cf56ddb2bc
Andreas Argelius <andreas@argeli.us>
2016-08-04 08:27:43 +0900

    Stuffs

--- js/angular-onsenui.js
+++ js/angular-onsenui.js
@@ -1,4 +1,4 @@
-/*! angular-onsenui.js for onsenui - v2.0.0-rc.15 - 2016-08-03 */
+/*! angular-onsenui.js for onsenui - v2.0.0-rc.15 - 2016-08-04 */
 /* Simple JavaScript Inheritance
  * By John Resig http://ejohn.org/
  * MIT Licensed.
